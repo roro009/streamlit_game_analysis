b5a5daf25cb65a8e10f80628f670e7b302358700
roro009 <113787389+roro009@users.noreply.github.com>
2023-11-20 22:16:11 -0500

    Add files via upload

---
+++ st.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[19]:
+
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+
+# In[20]:
+
+
+game_details = pd.read_csv('C:/Users/rohan/OneDrive/Desktop/INTRO TO INFORMATICS/LAB/Data_for_project/archive/games_details.csv')
+
+
+# In[27]:
+
+
+subset_data = game_details.sample(n=500)  # Adjust the sample size based on your dataset size
+
+def visualize_correlation():
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(x='FG_PCT', y='PTS', data=subset_data, alpha=0.7)
+    plt.title('Correlation between FG_PCT and PTS')
+    plt.xlabel('Field Goal Percentage (FG_PCT)')
+    plt.ylabel('Points Scored (PTS)')
+    st.pyplot()
+
+def main():
+    st.title("Game Analysis")
+    st.header("What is the correlation between the field goal percentage (FG_PCT) and the points scored (PTS) by players?")
+
+    st.header('Correlation Visualization')
+    visualize_correlation()
+
+
+    st.header('Subset of Data')
+    st.dataframe(subset_data.head())
+
+if __name__ == '__main__':
+    main()
+
